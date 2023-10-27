db5c32280b70e04b3d29da3bb87259654b0687fb
Orim12 <miro.vaassen@gmail.com>
2023-10-27 10:51:49 +0200

    Create opdracht3.py

---
+++ opdracht3.py
@@ -0,0 +1,7 @@
+nummer = 10
+if numme == 10:
+    print("De variabele is gelijk aan 10")
+elif numer == 50:
+    print("De variabele is gelijk aan 50")
+else:
+    print("De variabele is niet gelijk aan 10")
