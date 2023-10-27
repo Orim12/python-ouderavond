1c24b6c4eca21636d35391008551137e1e514af0
Orim12 <miro.vaassen@gmail.com>
2023-10-27 10:59:42 +0200

    Create opdracht4.py

---
+++ opdracht4.py
@@ -0,0 +1,9 @@
+nummer = 10
+if numme == 10:
+    print("De variabele is gelijk aan 10")
+elif numer == 50:
+    print("De variabele is gelijk aan 50")
+elif nummer == ?:
+    print("De variabele is gelijk aan ?")
+else:
+    print("De variabele is niet gelijk aan 10")
